0e874367187583bad4184229ea95baa449ad52eb
Innocent Asiimwe <asiimwe@outlook.com>
2017-05-18 00:29:56 +0300

    adding the donor model

--- Grants/models.py
+++ Grants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Donor(models.Model):
+    name = models.CharField(max_length = 200)
+    physical_address = models.CharField(max_length)
+    email_address = models.CharField(max_length)
+
+    def __str__(self):
+        return self.name
+
+class SupportOffice(models.Model):
+    name = 
