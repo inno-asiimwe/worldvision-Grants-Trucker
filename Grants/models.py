2055a8f889760b9ef1188385a80b6388a5e266a2
Innocent Asiimwe <asiimwe@outlook.com>
2017-05-18 02:13:28 +0300

    adding the project class to model.py

--- Grants/models.py
+++ Grants/models.py
@@ -27,3 +27,24 @@ class Payment(models.Model):
     payment_text = models.CharField(max_length = 200)
     pay_date = models.DateTimeField('Payment Date')
     amount = IntegerField('Amount paid')
+
+    def __str__(self):
+        return self.payment_text
+
+class Project(models.Model):
+    name = models.CharField(max_length = 200)
+    project_identifier = models.CharField(max_length = 200)
+    donor = models.ForeignKey(Donor, on_delete = models.CASCADE)
+    support_office = models.ForeignKey(SupportOffice, on_delete = models.CASCADE)
+    start_date = models.DateTimeField('Start date')
+    end_date = models.DateTimeField('End Date')
+    status = models.CharField(max_length =  50)
+    description = models.CharField(max_length = 200)
+    expenditure =  models.ForeignKey(Expenditure, on_delete = models.CASCADE)
+    payment = models.ForeignKey(Expenditure, on_delete = models.CASCADE)
+    grant_amount = models.IntegerField('Grant Amount')
+
+    def __str__(self):
+        return self.name
+
+        
