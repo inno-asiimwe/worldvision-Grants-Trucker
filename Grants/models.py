3a914c2513cf9f8095457ef4e4060b386638e7b4
Innocent Asiimwe <asiimwe@outlook.com>
2017-05-18 00:34:02 +0300

    adding the supportOffice class to models

--- Grants/models.py
+++ Grants/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 class Donor(models.Model):
     name = models.CharField(max_length = 200)
-    physical_address = models.CharField(max_length)
-    email_address = models.CharField(max_length)
+    physical_address = models.CharField(max_length = 200)
+    email_address = models.CharField(max_length = 200)
 
     def __str__(self):
         return self.name
 
 class SupportOffice(models.Model):
-    name = 
+    name = models.CharField(max_length = 200)
+    location = models.CharField(max_length = 200)
+
+    def __str__(self):
+        return self.name
